902db8d6ebf8c3df8317afc0368e621c05fadb96
davidmkhn <mihnodavid26032007@gmail.com>
2025-11-19 04:08:23 +0200

    added user Class added Water class inheritence of Dish class added addWater & totalWater methods to kbzhu_tracker class in kbzhu.js updated tests in kbzhu.js

--- Classes/kbzhu/kbzhu.js
+++ Classes/kbzhu/kbzhu.js
@@ -1,6 +1,7 @@
 class Dish{
-    constructor (name, calories, proteins, fats, carbs, date = new Date()){
+    constructor (name, weight, calories, proteins, fats, carbs, date = new Date()){
         this.name = name;
+        this.weight = weight;
         this.calories = calories;
         this.proteins = proteins;
         this.fats = fats;
@@ -8,17 +9,33 @@ class Dish{
         this.date = date;
     }
 }
+class Water extends Dish{
+     constructor(volume, date){
+     super("Вода", 0, 0, 0, 0, 0, date);
+     this.volume = volume;
+     }
+}
+
+
 
 class kbzhu_tracker 
 {
  constructor() {
+ this.goal;
  this.cpfc = [];
+ this.waterArr = [];
  }
 
-addFood(name, calories, proteins, fats, carbs, date)
+
+addFood(name, weight, calories, proteins, fats, carbs, date)
 {
- this.cpfc.push(new Dish(name, calories, proteins, fats, carbs, date));
+ this.cpfc.push(new Dish(name, weight, calories, proteins, fats, carbs, date));
 }
+addWater(name, volume, date)
+{
+ this.waterArr.push(new Water(name, volume, date));
+}
+
 
  TotalCPFC()
  {
@@ -40,6 +57,23 @@ return this.cpfc.filter(dish =>
 );
 }
 
+TotalWater(){
+    const now = new Date();
+
+    return this.waterArr.filter(water => 
+    water.date.getFullYear() === now.getFullYear() &&
+    water.date.getMonth() === now.getMonth() &&
+    water.date.getDate() === now.getDate()
+    )
+    .reduce(
+        (totalWater, water) => {
+        totalWater.volume += water.volume;
+        return totalWater;
+        },
+        {volume: 0}
+    )
+}
+
 removeDish(name){
     this.cpfc = this.cpfc.filter(dish => dish.name !== name);
     
@@ -51,12 +85,21 @@ removeDish(name){
 }
 
 const KBZHU = new kbzhu_tracker;
- KBZHU.addFood("Котлета", 34, 56, 76, 91);
- KBZHU.addFood("Суп", 100, 4, 30, 66);
+const Bot1 = new User("Джейк", "Чоловік", 84, 186, 34, 1.25);
+
+KBZHU.goal = Bot1.goalCPFC;
+console.log(KBZHU.goal);
+
+KBZHU.addWater(250);
+console.log(KBZHU.TotalCPFC());
+console.log(KBZHU.TotalWater());
+ KBZHU.addFood("Котлета", 200, 34, 56, 76, 91);
+ KBZHU.addFood("Суп", 34, 100, 4, 30, 66);
  console.log(KBZHU.TotalCPFC());
  console.log(KBZHU.cpfc);
   KBZHU.removeDish("Суп");
   console.log(KBZHU.cpfc);
 KBZHU.Clear();
 console.log(KBZHU.TotalCPFC());
+
 
